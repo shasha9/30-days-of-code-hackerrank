aa5150618453e64fd163830504aa34e087e15c4c
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-05 21:03:39 +0530

    Update Day_03_IntroToConditionalStatements.py

--- Day_03_IntroToConditionalStatements.py
+++ Day_03_IntroToConditionalStatements.py
@@ -3,8 +3,8 @@
 #Given an integer, , perform the following conditional actions:
 #If  is odd, print Weird
 #If  is even and in the inclusive range of  to , print Not Weird
-If  is even and in the inclusive range of  to , print Weird
-If  is even and greater than , print Not Weird#
+#If  is even and in the inclusive range of  to , print Weird
+#If  is even and greater than , print Not Weird#
 
 n=int(input())
 if n%2!=0:
