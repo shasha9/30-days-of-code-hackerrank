f911e16d2cca73d2c6fa85ec1e431d40452ace0f
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-05 21:07:05 +0530

    Update Day_02_Operators.py

--- Day_02_Operators.py
+++ Day_02_Operators.py
@@ -1,3 +1,6 @@
+#Task
+#Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost.
+
 mealCost = float(input())
 
 tipPercent = int(input())
