d4877f217501f789d5a55ba97bf3652159a7c6d3
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-06 21:09:05 +0530

    Update Day_04_ClassVSInstance.py

--- Day_04_ClassVSInstance.py
+++ Day_04_ClassVSInstance.py
@@ -1,3 +1,10 @@
+#Task
+# Write a person class with an instance variable age and a constructor that takes an integer initiated as a parameter the constructor much to sign initial age to age after confirming the argument past as initial age is not negative past as initially is the constructor should set age 20 and print age is not valid setting age to 0 in addition you must write the following instance methods year passes should increase the age instance variable-I am I also perform the following condition elections if it is less than 13 print you are young age is greater than equal to 13 and you just listen 18 print you are a teenager otherwise friend you are old
+
+
+
+
+
 class Person:
     age=0
     def __init__(self,initialAge):
