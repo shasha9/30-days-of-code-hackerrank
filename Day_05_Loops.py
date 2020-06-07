ac97d6f77aa48fc9cf005806c8b644cd5f3224f1
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-07 19:49:16 +0530

    Update Day_05_Loops.py

--- Day_05_Loops.py
+++ Day_05_Loops.py
@@ -2,3 +2,11 @@
 #Given an integer,n, print its first 10 multiples. 
 #Each multiple nxi  (where 1<=i<=10 )should be printed on a new line in the form: n x i = result.
 
+#!/bin/python3
+
+import sys
+
+
+N = int(input().strip())
+for i in range(1,11):
+    print(str(N) + " x " + str(i) + " = " + str(N*i))
