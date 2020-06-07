f51e468e44a97831b0156c7514acb14f30bdc640
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-07 19:48:08 +0530

    Update Day_05_Loops.py

--- Day_05_Loops.py
+++ Day_05_Loops.py
@@ -1,3 +1,4 @@
 #Task
 #Given an integer,n, print its first 10 multiples. 
-Each multiple nxi  (where ) should be printed on a new line in the form: n x i = result.
+#Each multiple nxi  (where 1<=i<=10 )should be printed on a new line in the form: n x i = result.
+
