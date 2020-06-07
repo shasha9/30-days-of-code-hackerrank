c01eefa3b9786a5c5c52212bd0c22459b4cd2a8e
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-07 19:51:15 +0530

    Update Day_05_Loops.py

--- Day_05_Loops.py
+++ Day_05_Loops.py
@@ -4,9 +4,14 @@
 
 #!/bin/python3
 
+import os
+import random
+import re
 import sys
 
 
-N = int(input().strip())
-for i in range(1,11):
-    print(str(N) + " x " + str(i) + " = " + str(N*i))
+
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1,11):
+        print(n,"x",i,"=",(i*n))
