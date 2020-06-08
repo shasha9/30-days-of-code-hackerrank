385b9e1c3c03ef9c531ec6dd4ae3ed1f4bfa1f4d
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-09 01:30:45 +0530

    Update Day_06_Let'sReview.py

--- Day_06_Let'sReview.py
+++ Day_06_Let'sReview.py
@@ -1,7 +1,6 @@
 #Task
 #Given a string, S , of length N that is indexed from 0 to N-1, 
 #print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line.
-
 if __name__ == '__main__':
 
     test = int(input())
@@ -17,5 +16,4 @@ if __name__ == '__main__':
         for i in range (len(s)):
             if i%2 == 1:
                 print(s[i],sep = "", end = "")
-        
         print("")
