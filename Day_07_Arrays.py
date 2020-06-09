a36648371df0cdc3a74488c3b9892e236f83107e
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-09 20:02:43 +0530

    Update Day_07_Arrays.py

--- Day_07_Arrays.py
+++ Day_07_Arrays.py
@@ -1,2 +1,11 @@
 #Task
 #Given an array, A , of N integers, print A's elements in reverse order as a single line of space-separated numbers.
+
+import sys
+
+
+n = int(input().strip())
+arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+arr.reverse()
+for i in range(n):
+    print(arr[i], end=" ")
