25646b3c4b579dae00963e63c07817e881b05b6c
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-10 20:11:39 +0530

    Day 7 : Arrays

--- Day_07_Arrays.py
+++ Day_07_Arrays.py
@@ -5,7 +5,7 @@ import sys
 
 
 n = int(input().strip())
-arr = [int(arr_temp) for arr_temp in input().strip().split(' ')]
+arr =[int(arr_temp) for arr_temp in input().strip().split(' ')]
 arr.reverse()
 for i in range(n):
     print(arr[i], end=" ")
