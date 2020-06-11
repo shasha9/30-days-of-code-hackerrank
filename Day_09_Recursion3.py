04b5aaf37c0350b05f373ee605d1a49f5baa64bc
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-11 19:58:56 +0530

    Day : 9 Recursion 3

---
+++ Day_09_Recursion3.py
@@ -0,0 +1,11 @@
+#Task
+#Write a factorial function that takes a positive integer,N as a parameter and prints the result of N! ( factorial).
+
+def factorial(n): 
+      
+    
+    return 1 if (n==1 or n==0) else n * factorial(n - 1)
+  
+
+num = int(input())
+print( factorial(num)) 
