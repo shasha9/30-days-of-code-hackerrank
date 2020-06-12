2dcd222b0b94caea55e4cc1c1df2ef197562e48a
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-12 20:03:01 +0530

    Update Day_10_Binary_Numbers.py

--- Day_10_Binary_Numbers.py
+++ Day_10_Binary_Numbers.py
@@ -1,3 +1,24 @@
 #Task
 #Given a base-10 integer,n, convert it to binary (base-2). 
 #Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation.
+
+import sys
+
+n = int(input().strip())
+
+binary = list(bin(n)[2:])
+#print (binary)
+
+count = 0
+max_count = 0
+for i in binary:
+    if (i == '1'):
+        count += 1
+    else:
+        if count > max_count:
+            max_count = count
+        count = 0
+if count > max_count:
+    max_count = count
+print (max_count)
+
