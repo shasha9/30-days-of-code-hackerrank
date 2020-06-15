51a32bb506de13153b6f0ca36aa91fba82824ea0
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-15 20:54:42 +0530

    Day : 13 Abstract Classes

--- Day_13_Abstract_Classes.py
+++ Day_13_Abstract_Classes.py
@@ -13,7 +13,7 @@
 
 
 #Write MyBook class
-class MyBook(Book):
+class MyBook(Book) :
     def __init__(self,title,author,price):
         Book.__init__(self,title,author)
         self.price = price
