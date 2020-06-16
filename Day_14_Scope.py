b1a1ae9612b5f0fc286e0adb85b031761c3dfaa1
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-16 20:32:07 +0530

    Update Day_14_Scope.py

--- Day_14_Scope.py
+++ Day_14_Scope.py
@@ -2,3 +2,10 @@
 
 #A class constructor that takes an array of integers as a parameter and saves it to the elements instance variable.
 #A computeDifference method that finds the maximum absolute difference between any 2 numbers in N and stores it in the maximumDifference instance variable.
+
+   def computeDifference(self):
+        self.maximumDifference = 0
+        for x in range(len(a)):
+            for y in range(x,len(a)):
+                if abs(a[x] - a[y]) > self.maximumDifference:
+                    self.maximumDifference = abs(a[x] - a[y])
