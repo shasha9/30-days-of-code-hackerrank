cc4644ababce8a68d85412763f5f40ce114c898a
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-17 20:38:11 +0530

    Update Day_15_Linked_List.py

--- Day_15_Linked_List.py
+++ Day_15_Linked_List.py
@@ -2,3 +2,5 @@
 #Complete the insert function in your editor so that it creates a new Node (pass data as the Node constructor argument) 
 #and inserts it at the tail of the linked list referenced by the head parameter. 
 #Once the new node is added, return the reference to the head node.
+
+def insert(self,head,data): 
