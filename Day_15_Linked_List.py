b018f2e781f63b4dce6ad8a881224b36aeb6cf27
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-17 20:48:43 +0530

    Update Day_15_Linked_List.py

--- Day_15_Linked_List.py
+++ Day_15_Linked_List.py
@@ -3,13 +3,30 @@
 #and inserts it at the tail of the linked list referenced by the head parameter. 
 #Once the new node is added, return the reference to the head node.
 
-def insert(self,head,data): 
-     n= Node(data)
-        if head:
-            current = headL
-            while current.next:
-                current = current.next
-            current.next = n
-            return head
-        else:
-            return n
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def insert(self,head,data):
+        if (head == None):
+            head = Node(data)
+        elif (head.next == None):
+            head.next = Node(data)
+        else: 
+            self.insert(head.next, data)
+        return head
+
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+mylist.display(head); 	 
