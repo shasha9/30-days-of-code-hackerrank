2a55f493cadac0f6db12df99416a17b3b18b21c4
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-19 20:24:29 +0530

    Update Day_17_More_Exceptions.py

--- Day_17_More_Exceptions.py
+++ Day_17_More_Exceptions.py
@@ -1,1 +1,4 @@
-
+#Task
+#Write a Calculator class with a single method: int power(int,int). 
+#The power method takes two integers, n and p, as parameters and returns the integer result of n to the power p.
+#If either n or p is negative,then the method must throw an exception with the message: n and p should be non-negative.
