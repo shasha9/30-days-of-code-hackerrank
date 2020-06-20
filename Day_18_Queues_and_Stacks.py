a9fa8f0c023e081b297966a8f732bf184dec6679
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-20 20:03:45 +0530

    Update Day_18_Queues_and_Stacks.py

--- Day_18_Queues_and_Stacks.py
+++ Day_18_Queues_and_Stacks.py
@@ -1,1 +1,11 @@
-
+    def __init__(self):
+        self.queue = []
+        self.stack = []
+        
+    def pushCharacter(self, ch):
+        self.stack.insert(0, ch)
+    def enqueueCharacter(self, ch):
+        self.queue.append(ch)
+    def popCharacter(self):
+     
+    
