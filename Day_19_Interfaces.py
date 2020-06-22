14a39c78c224a357ae8029dd4f2f7832b60917ce
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-22 20:31:36 +0530

    Day : 19 Interfaces

--- Day_19_Interfaces.py
+++ Day_19_Interfaces.py
@@ -1,5 +1,5 @@
 #Task
-#Complete the implementation of Calculator class, which implements the AdvancedArithmetic interface. 
+#Complete the implementation of Calculator class which implements the AdvancedArithmetic interface. 
 #The implementation for the divisorSum(n) method must return the sum of all divisors of n.
 
 class AdvancedArithmetic(object):
