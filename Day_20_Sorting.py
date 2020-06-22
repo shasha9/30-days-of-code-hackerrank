bdb94988e88f02ec0577bc8ede06ed377c84b8c9
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-22 20:53:59 +0530

    Update Day_20_Sorting.py

--- Day_20_Sorting.py
+++ Day_20_Sorting.py
@@ -1,2 +1,23 @@
 #Task
 #Sort a given array using Bubble Sort algorithm.
+
+import sys
+
+
+n = int(input().strip())
+a = [int(a_temp) for a_temp in input().strip().split(' ')]
+
+totalswaps=0
+for i in range(0,len(a)):
+    nbswaps=0
+    for i in range(0,len(a)-1):
+        if a[i]>a[i+1]:
+            a[i],a[i+1] = a[i+1],a[i]
+            nbswaps+=1
+    totalswaps+=nbswaps
+    if nbswaps==0:
+        break;
+         
+print("Array is sorted in",totalswaps,"swaps.")  
+print("First Element:",str(a[0]))
+print("Last Element:",str(a[-1]))
