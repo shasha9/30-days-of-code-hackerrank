dc0b2c28b3b3a8ea96b0c9c0e3f7fd24002485b4
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-24 21:06:08 +0530

    Day : 21 Generics

--- Day_21_Generics.py
+++ Day_21_Generics.py
@@ -1,5 +1,5 @@
 #Task
-#Write a single generic function named printArray; this function must take an array of generic elements as a parameter
+#Write a single generic function named printArray;this function must take an array of generic elements as a parameter.
 
 from typing import TypeVar
 
