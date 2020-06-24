4de9240e58807f9c2a78fb3da6fb0ee3b55de1ed
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-24 21:03:40 +0530

    Update Day_21_Generics.py

--- Day_21_Generics.py
+++ Day_21_Generics.py
@@ -1,12 +1,12 @@
-/**
-    *    Method Name: printArray
-    *    Print each element of the generic array on a new line. Do not return anything.
-    *    @param A generic array
-    **/
-    
-    // Write your code here
-    public static <Element> void printArray(Element[] array) {
-        for (Element element :array) {
-            System.out.println(element);
-        }
-    }
+from typing import TypeVar
+
+Element = TypeVar("Element")
+
+def printArray(array: [Element]):
+    for element in array:
+        print(element)
+
+vInt = [1, 2, 3]
+vString = ["Hello", "World"]
+printArray(vInt)
+printArray(vString)
