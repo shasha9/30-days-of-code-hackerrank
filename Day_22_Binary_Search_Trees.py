7dd42acea61ebcd6e4c9a88e8538669614a60282
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-24 21:15:34 +0530

    Day : 22 Binary Search Trees

--- Day_22_Binary_Search_Trees.py
+++ Day_22_Binary_Search_Trees.py
@@ -1,6 +1,6 @@
 #Task
 #The height of a binary search tree is the number of edges between the tree's root and its furthest leaf. 
-#You are given a pointer,root,pointing to the root of a binary search tree. Complete the getHeight function provided in your editor so that it returns the height of the binary search tree.
+#You are given a pointer,root, pointing to the root of a binary search tree. Complete the getHeight function provided in your editor so that it returns the height of the binary search tree.
 
 def getHeight(self,root):
         
