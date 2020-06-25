79ce6989a56707c070e50fd46a57fade4b4b777b
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-25 21:20:23 +0530

    Update Day_23_BST_LevelOrderTraversal.py

--- Day_23_BST_LevelOrderTraversal.py
+++ Day_23_BST_LevelOrderTraversal.py
@@ -1,1 +1,11 @@
-
+def levelOrder(self,root):
+        #BFS
+        aVisiter=[root]
+        while len(aVisiter)>0:
+            for i in range(len(aVisiter)):
+                el=aVisiter.pop(0)
+                print(el.data, end=" ")
+                if el.left:
+                    aVisiter.append(el.left)
+                if el.right:
+                    aVisiter.append(el.right)
