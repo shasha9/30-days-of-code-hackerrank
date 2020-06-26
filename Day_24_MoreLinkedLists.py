1d3923855a9091265a03900a5e064da2773b8cc8
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-26 19:39:53 +0530

    Update Day_24_MoreLinkedLists.py

--- Day_24_MoreLinkedLists.py
+++ Day_24_MoreLinkedLists.py
@@ -1,1 +1,19 @@
-
+def removeDuplicates(self,head):
+        #Write your code here
+        if head!=None:
+            cur=head
+            nex=cur.next
+            pre=cur
+            while(cur!=None):
+                while(nex!=None):                    
+                    if nex.data==cur.data:                       
+                        pre.next=nex.next
+                    else:
+                        pre=pre.next
+                    nex=nex.next
+                cur=cur.next
+                if cur!=None:
+                    nex=cur.next
+                    pre=cur
+        return head
+  
