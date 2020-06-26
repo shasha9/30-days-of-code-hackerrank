8f5f63d078761df8f34d999adc25ce823d0975ae
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-26 19:44:35 +0530

    Day : 24 More Linked Lists

--- Day_24_MoreLinkedLists.py
+++ Day_24_MoreLinkedLists.py
@@ -2,7 +2,7 @@
 #A Node class is provided. A Node object has an integer data field,data, and a Node instance pointer,next , pointing to another node (i.e.: the next node in a list).
 
 #A removeDuplicates function is declared, which takes a pointer to the head node of a linked list as a parameter. 
-#Complete the removeDuplicates so that it deletes any duplicate nodes from the list and returns the head of the updated list.
+#Complete the removeDuplicates function so that it deletes any duplicate nodes from the list and returns the head of the updated list.
 
 
 def removeDuplicates(self,head):
