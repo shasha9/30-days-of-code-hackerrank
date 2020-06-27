1706069f660923b05150d3eab208c8c3ce906533
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-27 20:52:25 +0530

    Update Day_25_RunningTimeAndComplexity.py

--- Day_25_RunningTimeAndComplexity.py
+++ Day_25_RunningTimeAndComplexity.py
@@ -1,2 +1,26 @@
 #Task
 #Given a number,n, determine and print whether it's prime or not prime .
+
+import math
+        
+def is_prime(number):
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    x = 5
+    while x*x <= n:
+        if n % x == 0 or n % (x+2) ==0:
+            return False
+        x = x + 6
+    return True
+
+T = int(input())
+for i in range(0,T):
+    n=int(input())
+    if is_prime(n):
+        print("Prime")
+    else:
+        print("Not prime")
