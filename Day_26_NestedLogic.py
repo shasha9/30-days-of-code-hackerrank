1502d08e59a8de5bc62d22263193262dc08ecc47
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-28 20:29:10 +0530

    Update Day_26_NestedLogic.py

--- Day_26_NestedLogic.py
+++ Day_26_NestedLogic.py
@@ -16,3 +16,11 @@ def calculate_fine(actual,expected):
                 return 15 * (actual[0]-expected[0])
             else:
                 return 0
+
+actual = input().split()
+expected = input().split()
+for i in range(3):
+    actual[i] = int(actual[i])
+    expected[i] = int(expected[i])
+
+print(calculate_fine(actual, expected))
