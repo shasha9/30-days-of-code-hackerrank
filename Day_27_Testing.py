002803baf38d71acdb293ef97eab9b93c70dc02f
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-29 20:22:55 +0530

    Update Day_27_Testing.py

--- Day_27_Testing.py
+++ Day_27_Testing.py
@@ -6,4 +6,15 @@ class TestDataEmptyArray(object):
     def get_array():
         # complete this function
         return []
+ class TestDataUniqueValues(object):
+
+    data = set()
+    while len(data) < 10:
+        data.add(randint(0, 100))
+
+    @staticmethod
+    def get_array():
+        # complete this function
+        data = TestDataUniqueValues.data
+        return list(data)
 
