36407cb97328779488c15de15e7d97d1f7f6dc3d
Shasha Sami <shasha.sami91199@gmail.com>
2020-06-30 18:14:12 +0530

    Day : 28 RegEx, Patterns ,Intro to Databases

--- Day_28_RegExPatternsDatabases.py
+++ Day_28_RegExPatternsDatabases.py
@@ -3,7 +3,7 @@
 #which has the attributes First Name and Email ID. Given N rows of data simulating the Emails table, print an alphabetically-ordered list of people whose email address ends in @gmail.com .
 
 
-import sys, re
+import sys,re
 
 names = []
 pattern = re.compile('@gmail.com$')
