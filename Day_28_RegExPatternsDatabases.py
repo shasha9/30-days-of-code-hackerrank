3be6d5783c797145fe9d58d41b055b10f01e05aa
Shasha Sami <shasha.sami91199@gmail.com>
2020-07-01 20:56:11 +0530

    Day : 28 RegEx,Patterns and Databases

--- Day_28_RegExPatternsDatabases.py
+++ Day_28_RegExPatternsDatabases.py
@@ -1,6 +1,6 @@
 #Task
 #Consider a database table, Emails,
-#which has the attributes First Name and Email ID. Given N rows of data simulating the Emails table, print an alphabetically-ordered list of people whose email address ends in @gmail.com .
+#which has the attributes First Name and Email ID.Given N rows of data simulating the Emails table, print an alphabetically-ordered list of people whose email address ends in @gmail.com .
 
 
 import sys,re
