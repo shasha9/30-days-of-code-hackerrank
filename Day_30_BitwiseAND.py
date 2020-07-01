6acc2791e9de46fc7c0ac005a0290a2509bf2354
Shasha Sami <shasha.sami91199@gmail.com>
2020-07-01 20:53:39 +0530

    Day : 30 Bitwise AND

--- Day_30_BitwiseAND.py
+++ Day_30_BitwiseAND.py
@@ -1,6 +1,6 @@
 #Task
 #Given set S={1,2,3,...N}.Find two integers,A and B(where A<B)
-#from set S such that the value of A and B is the maximum possible and also less than a given integer,K.In this case,&represents the bitwise AND operator.
+#from set S such that the value of A and B is the maximum possible and also less than a given integer,K.In this case,& represents the bitwise AND operator.
 
 import sys
 import math
